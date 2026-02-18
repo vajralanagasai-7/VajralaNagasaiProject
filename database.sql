d830c458bcc1528e49e7d1f7c754f71f8721d112
vajralanagasai-7 <nagasaitattvacodetech@gmail.com>
2026-02-18 12:24:42 +0530

    updated code

--- database.sql
+++ database.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE college_db;
 USE college_db;
 
-CREATE TABLE students (
+CREATE TABLE students  (
     student_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50),
+    name VARCHAR(60),
     course VARCHAR(50),
     year INT
-);
+);  
